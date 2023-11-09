880a4177b7349bcf72e966b69485313534bb9f83
techByKyle <kyle@techbykyle.com>
2023-11-09 14:16:04 -0700

    Added https to the webpack dev server. It will share the same env vars as the main app for the certs.

--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,25 @@ const { ModuleFederationPlugin } = require('webpack').container
 const pkg = require('./package.json')
 const exposes = require('./exposes.json')
 
+const IOT_API_SSL_CRT = process.env.IOT_API_SSL_CRT || '/home/user/ssl/cert.crt'
+const IOT_API_SSL_KEY = process.env.IOT_API_SSL_KEY  || '/home/user/ssl/key.key'
+
 module.exports = {
     entry: './src/index.js',
     devServer: {
+        server: {
+            type: 'https',
+            options: {
+                key: IOT_API_SSL_KEY,
+                cert: IOT_API_SSL_CRT,
+                requestCert: false,
+                headers: {
+                    'Access-Control-Allow-Origin': '*',
+                    'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+                    "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
+                },
+            },
+        },
         host: '0.0.0.0',
         port: 3001
     },
